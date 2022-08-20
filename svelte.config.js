8e1e00ca88914700076d2f951d5f6a59caae88d1
trivialonly <miraco111@163.com>
2022-08-20 17:15:07 +0800

    update site

--- svelte.config.js
+++ svelte.config.js
@@ -12,11 +12,16 @@
 
 // export default config;
 
-
+import { readFileSync } from "fs";
 import adapter from "@sveltejs/adapter-static"; 
-// was "@sveltejs/adapter-auto"
 
-const dev = "production" === "development";
+const { subdirectory } = JSON.parse(readFileSync("package.json", "utf8"));
+const dev = process.env.NODE_ENV === "development";
+const dir = subdirectory || "";
+const prefix = dir.startsWith("/") ? "" : "/";
+const base = dev || !dir ? "" : `${prefix}${dir}`;
+
+// const dev = "production" === "development";
 
 /** @type {import(""@sveltejs/kit").Config} */
 const config = {
@@ -25,7 +30,10 @@ const config = {
             pages: "docs",
 						assets: "docs",
 						fallback: "index.html",
-        }),
+				}),
+				paths: {
+					base
+				}
     }
 };
 
